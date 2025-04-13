0a72ab8c00cc7d83581003f2fc6bdd810d642500
tundish <tundish@gigeconomy.org.uk>
2025-04-13 14:45:38 +0100

    Tidy.

--- sagids/test/test_grid.py
+++ sagids/test/test_grid.py
@@ -24,18 +24,26 @@ import unittest
 import dataclasses
 from fractions import Fraction
 import random
+import turtle
 
 
 class Grid:
 
     @dataclasses.dataclass
-    class Register:
+    class Cell:
         value: Fraction = None
+        parent: "Grid" = None
+
+    @dataclasses.dataclass
+    class Marker:
+        value: Fraction = None
+        parent: "Grid" = None
+        cell: "Cell" = None
 
     @classmethod
-    def build_registers(cls, k=4):
+    def build_markers(cls, k=4):
         return [
-            cls.Register(value=v)
+            cls.Marker(value=v)
             for v in random.sample(
                 [Fraction(n, 9) for n in [0, 1, 2, 4, 5, 7, 8]],
                 k
@@ -44,25 +52,30 @@ class Grid:
 
     @classmethod
     def build(cls, n_sectors=4, n_regions=4):
-        marks = cls.build_registers(k=n_sectors)
-        return cls(marks)
+        markers = cls.build_markers(k=n_sectors)
+        return cls(markers)
 
-    def __init__(self, marks: list = None, slots: list = None):
-        self.marks = marks or []
-        self.slots = slots or []
+    def __init__(self, markers: list = None, cells: list = None):
+        self.markers = markers or []
+        for mark in self.markers:
+            mark.parent = self
+
+        self.cells = cells or []
+        for cell in self.cells:
+            cell.parent = self
 
 
 class GridTests(unittest.TestCase):
 
-    def test_registers(self):
+    def test_markers(self):
         for n in range(8):
             with self.subTest(n=n):
-                rv = Grid.build_registers(n)
+                rv = Grid.build_markers(n)
                 self.assertEqual(len(rv), n)
                 self.assertEqual(len({i.value for i in rv}), n)
 
     def test_init(self):
         grid = Grid.build()
-        self.assertEqual(len(grid.marks), 4)
+        self.assertEqual(len(grid.markers), 4)
         print(f"{grid=}")
 
