ad22c2532caee237b94ed2734b07958d6ed86786
tundish <tundish@gigeconomy.org.uk>
2025-09-14 10:34:46 +0100

    Add test module for board#.

---
+++ sagids/test/test_board.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python3
+# encoding: UTF-8
+
+# This file is part of SaGiDS.
+
+# SaGiDS is free software: You can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License,
+# or (at your option) any later version.
+
+# SaGiDS is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+
+# You should have received a copy of the
+# GNU General Public License along with SaGiDS.
+# If not, see <https://www.gnu.org/licenses/>.
+
+
+import unittest
+
+from sagids.board import Board
+from sagids.coordinates import Coordinates
+
+
+class BoardTests(unittest.TestCase):
+    pass
